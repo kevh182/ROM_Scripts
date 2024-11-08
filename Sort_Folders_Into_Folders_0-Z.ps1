6fecbfc0b89cff206da7e5967df8bbff6f267526
kevh182 <154713523+kevh182@users.noreply.github.com>
2024-11-08 09:13:54 -0500

    Create Sort_Folders_Into_Folders_0-Z.ps1
    Sort folders into folders based on the first character of the folder name.  Organizes 0-9, A-Z

---
+++ Sort_Folders_Into_Folders_0-Z.ps1
@@ -0,0 +1,41 @@
+﻿$ErrorActionPreference = "SilentlyContinue"
+
+Do {
+# Set the source folder path
+$sourceFolder = Read-Host "Enter ROM Path"
+
+# Get all folders in the source folder
+$folders = Get-ChildItem -Path $sourceFolder
+
+# Loop through each folder and move it to the appropriate folder
+
+foreach ($folder in $folders) {
+
+    # Get the first character of the folder name
+    $firstCharacter = $folder.Name.Substring(0, 1)
+
+    # Determine the destination folder based on the first character
+    if ($firstCharacter -match "[0-9]") {
+
+        $destinationPath = Join-Path $sourceFolder "0-9"
+
+    } else {
+
+        $destinationPath = Join-Path $sourceFolder $firstCharacter.ToUpper()
+
+    }
+
+    # Create the destination folder if it doesn't exist
+    if (!(Test-Path $destinationPath)) {
+
+        New-Item -ItemType Directory -Path $destinationPath
+
+    }
+
+    # Move the folders to the destination folder
+    Move-Item -Path $folder.FullName -Destination $destinationPath
+}
+
+Write-Host "Folders moved successfully!"
+
+} While ($true)
